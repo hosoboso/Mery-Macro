b552c728aac720e5e5f4b832dbd6487c03a7ceb3
hosoboso <139880091+hosoboso@users.noreply.github.com>
2024-07-18 21:37:40 +0900

    Update openfile.js

--- openfile.js
+++ openfile.js
@@ -7,26 +7,31 @@
 openFile Mery用 私家改造版
 
 これはakkun氏によるopenFileを個人用に書き換えたものです。
-選択したテキストをMery（別タブ）で開きます。
+選択したテキストをファイルとしてMery（別タブ）で開きます。
+相対パス・絶対パス対応。
 テキストエディタMeryで一応動きます。動作保証はできません。
 以下「＊」がついたコメントは私・hosobosoによる追記
 */
 
 //openFile
 //選択したテキストをファイル名として開く
- 
+
 var dir = document.FullName;
 var selected = document.selection.Text;
 var p = 0;	//親階層数
 
-//＊ファイルとして開く拡張子指定　以下で指定した拡張子のテキストを選択しても何も起きない
-//＊好みで追加・削除してください
-if(/\.(htm|html|shtml|css|js|xml)$/.test(selected)){
+if(selected.length == 0) {
+	alert("テキストを選択してください。");
+
+} else if (/\.(htm|html|shtml|css|js|xml)$/.test(selected)){
+// ＊ファイルとして開く拡張子指定
+// ＊上で指定した拡張子のテキストを選択して実行するとファイルを開きます
+// ＊好みで追加・削除してください
 
 	//選択文字列がフルパスならそのままオープン
 	if (selected.search(/^[a-z]:\\/i)>=0){
 		Editor.OpenFile(selected, meEncodingNone, meOpenAllowNewWindow);
-		//＊meOpenAllowNewWindowフラグをつけると元のファイルとは別タブ（か別ウィンドウ）で開きます
+		// ＊meOpenAllowNewWindowフラグをつけると元のファイルとは別タブ（か別ウィンドウ）で開きます
 	}else{
 		//親階層の検索
 		while( selected.search(/^\.\.\\/) != -1 ){
@@ -43,4 +48,6 @@ if(/\.(htm|html|shtml|css|js|xml)$/.test(selected)){
 		Editor.OpenFile((dir + '\\' + selected), meEncodingNone, meOpenAllowNewWindow);
 	}
 
+} else {
+	alert("選択テキストはファイルとして\n開くことができません。");
 }
